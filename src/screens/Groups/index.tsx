8e2bdc0d3331961be0b9c3cfb85eeb60d8892df6
Italo Araujo souza <italoasouzat.i@gmail.com>
2022-09-25 20:44:20 -0400

    Criando tela de groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import Header from '@components/Header';
+import Highlight from '@components/Highlight';
+import GroupCard from '@components/GroupCard';
+import ListEmpty from '@components/ListEmpty';
+import Button from '@components/Button';
 import React from 'react';
-import { View, Text } from 'react-native';
+
 
 import * as Style from './styles';
 
 const Groups: React.FC = () => {
+    const [groups, setGroups] = useState<string[]>([]);
     return (
         <Style.Container>
+            <Header />
+            <Highlight
+                title='Turmas'
+                subTitle='Jogue com sua turma'
+            />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message='Que tal cadastrar a primeira turma?'
+                    />
+                )}
+            />
+            <Button
+                title='Criar nova turma'
+            />
+
         </Style.Container>
     );
 }
