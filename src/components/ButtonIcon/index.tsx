9086952cd97ddb5d8d86720d442c6de5f7c72552
Italo Araujo souza <italoasouzat.i@gmail.com>
2022-09-25 21:13:42 -0400

    criando componente buttonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import { TouchableProps } from 'react-native-svg';
+import { MaterialIcons } from '@expo/vector-icons'
+import * as Style from './styles';
+
+type Props = TouchableProps & {
+    icon: keyof typeof MaterialIcons.glyphMap,
+    type?: Style.ButtonIconTypeStyleProps,
+}
+
+const ButtonIcon: React.FC<Props> = ({ icon,
+    type = "PRIMARY", ...rest }) => {
+    return (
+        <Style.Container>
+            <Style.Icon
+                name={icon}
+                type={type}
+            />
+        </Style.Container>
+    );
+}
+
+export default ButtonIcon;
