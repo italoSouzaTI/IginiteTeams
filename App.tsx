1e11c62a123eb7829259771016bc987e6330d669
Italo Araujo souza <italoasouzat.i@gmail.com>
2022-10-09 22:35:20 -0400

    finalizando storage

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import theme from './src/theme'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 //Screens
-import Groups from '@screens/Groups';
+import { Routes } from './src/routes';
 import Loading from "@components/Loading"
 export default function App () {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +16,7 @@ export default function App () {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
